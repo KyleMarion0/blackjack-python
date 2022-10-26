498ff770982977c1e6a79ae25aaf44a3c7a05a96
Kyle Marion <73936202+KyleMarion0@users.noreply.github.com>
2022-10-26 01:13:16 -0400

    Created a card calculation function and more.
    Also outputs cards and scores.

--- blackjack.py
+++ blackjack.py
@@ -6,12 +6,26 @@ def deal_card():
   card = random.choice(cards)
   return card
 
+def calculate_score(cards):
+  """Takes a list of cards and returns the score."""
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+  if 11 in cards and sum(cards) > 21:
+    cards.remove(11)
+    cards.append(1)
+  return sum(cards)
+
 user_cards = []
 computer_cards = []
 
 for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
+  user_cards.append(deal_card())
+  computer_cards.append(deal_card())
 
-print(user_cards)
-print(computer_cards)
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"Your cards: {user_cards}, Your score: {user_score}")
+print(f"Computers cards: {computer_cards}, Computers score: {computer_score}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    print('Game over!')
